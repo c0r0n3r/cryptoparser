81b3df9154f1d2a085c6da3234f6dd0a0de08e9b
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-04-07 20:12:02 +0200

    tls: Change cipher spec

---
+++ tests/tls/test_change_cipher_spec.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import six
+
+from cryptoparser.common.exception import NotEnoughData, InvalidValue
+
+from cryptoparser.tls.subprotocol import TlsChangeCipherSpecMessage, TlsChangeCipherSpecType
+
+
+class TestRecord(unittest.TestCase):
+    def test_error(self):
+        with six.assertRaisesRegex(self, InvalidValue, '0xff is not a valid TlsChangeCipherSpecType'):
+            # pylint: disable=expression-not-assigned
+            TlsChangeCipherSpecMessage.parse_exact_size(b'\xff'),
+
+        with self.assertRaises(NotEnoughData) as context_manager:
+            # pylint: disable=expression-not-assigned
+            TlsChangeCipherSpecMessage.parse_exact_size(b''),
+        self.assertEqual(context_manager.exception.bytes_needed, 1)
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsChangeCipherSpecMessage.parse_exact_size(b'\x01'),
+            TlsChangeCipherSpecMessage(TlsChangeCipherSpecType.CHANGE_CIPHER_SPEC)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            b'\x01',
+            TlsChangeCipherSpecMessage(TlsChangeCipherSpecType.CHANGE_CIPHER_SPEC).compose()
+        )
