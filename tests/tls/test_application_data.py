c0r0n3r/cryptolyzer

tls: Application data

tests/tls/test_application_data.py
@@ -1,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from crypton.tls.record import TlsContentType
+from crypton.tls.subprotocol import TlsApplicationDataMessage
+
+
+class TestRecord(unittest.TestCase):
+    _APPLICATION_DATA_MESSAGE_BYTES = b'\x01\x02\x03\x04'
+
+    def test_error(self):
+        pass
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsApplicationDataMessage.parse_exact_bytes(self._APPLICATION_DATA_MESSAGE_BYTES),
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            self._APPLICATION_DATA_MESSAGE_BYTES,
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES).compose()
+        )
+
+    def test_content_type(self):
+        self.assertEqual(TlsApplicationDataMessage.get_content_type(), TlsContentType.APPLICATION_DATA)

c0r0n3r/cryptoparser

tests/tls/test_application_data.py
@@ -1,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from cryptolyzer.tls.record import TlsContentType
+from cryptolyzer.tls.subprotocol import TlsApplicationDataMessage
+
+
+class TestRecord(unittest.TestCase):
+    _APPLICATION_DATA_MESSAGE_BYTES = b'\x01\x02\x03\x04'
+
+    def test_error(self):
+        pass
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsApplicationDataMessage.parse_exact_size(self._APPLICATION_DATA_MESSAGE_BYTES),
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            self._APPLICATION_DATA_MESSAGE_BYTES,
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES).compose()
+        )
+
+    def test_content_type(self):
+        self.assertEqual(TlsApplicationDataMessage.get_content_type(), TlsContentType.APPLICATION_DATA)