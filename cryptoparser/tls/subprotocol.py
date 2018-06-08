c0r0n3r/cryptolyzer

tls: Application data

crypton/tls/subprotocol.py
@@ -150,3 +150,24 @@ class TlsChangeCipherSpecMessage(TlsSubprotocolMessageBase):
 
     def __eq__(self, other):
         return self._change_cipher_spec_type == other._change_cipher_spec_type
+
+
+class TlsApplicationDataMessage(TlsSubprotocolMessageBase):
+    def __init__(self, data):
+        super(TlsApplicationDataMessage, self).__init__()
+
+        self.data = data
+
+    @classmethod
+    def get_content_type(cls):
+        return TlsContentType.APPLICATION_DATA
+
+    @classmethod
+    def _parse(cls, parsable_bytes):
+        return TlsApplicationDataMessage(parsable_bytes), len(parsable_bytes)
+
+    def compose(self):
+        return self.data
+
+    def __eq__(self, other):
+        return self.data == other.data

tests/tls/test_application_data.py
@@ -1,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from crypton.tls.record import TlsContentType
+from crypton.tls.subprotocol import TlsApplicationDataMessage
+
+
+class TestRecord(unittest.TestCase):
+    _APPLICATION_DATA_MESSAGE_BYTES = b'\x01\x02\x03\x04'
+
+    def test_error(self):
+        pass
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsApplicationDataMessage.parse_exact_bytes(self._APPLICATION_DATA_MESSAGE_BYTES),
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            self._APPLICATION_DATA_MESSAGE_BYTES,
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES).compose()
+        )
+
+    def test_content_type(self):
+        self.assertEqual(TlsApplicationDataMessage.get_content_type(), TlsContentType.APPLICATION_DATA)

c0r0n3r/cryptoparser

cryptoparser/tls/subprotocol.py
@@ -149,3 +149,24 @@ class TlsChangeCipherSpecMessage(TlsSubprotocolMessageBase):
 
     def __eq__(self, other):
         return self._change_cipher_spec_type == other._change_cipher_spec_type
+
+
+class TlsApplicationDataMessage(TlsSubprotocolMessageBase):
+    def __init__(self, data):
+        super(TlsApplicationDataMessage, self).__init__()
+
+        self.data = data
+
+    @classmethod
+    def get_content_type(cls):
+        return TlsContentType.APPLICATION_DATA
+
+    @classmethod
+    def _parse(cls, parsable):
+        return TlsApplicationDataMessage(parsable), len(parsable)
+
+    def compose(self):
+        return self.data
+
+    def __eq__(self, other):
+        return self.data == other.data

tests/tls/test_application_data.py
@@ -1,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from cryptoparser.tls.record import TlsContentType
+from cryptoparser.tls.subprotocol import TlsApplicationDataMessage
+
+
+class TestRecord(unittest.TestCase):
+    _APPLICATION_DATA_MESSAGE_BYTES = b'\x01\x02\x03\x04'
+
+    def test_error(self):
+        pass
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsApplicationDataMessage.parse_exact_size(self._APPLICATION_DATA_MESSAGE_BYTES),
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            self._APPLICATION_DATA_MESSAGE_BYTES,
+            TlsApplicationDataMessage(data=self._APPLICATION_DATA_MESSAGE_BYTES).compose()
+        )
+
+    def test_content_type(self):
+        self.assertEqual(TlsApplicationDataMessage.get_content_type(), TlsContentType.APPLICATION_DATA)