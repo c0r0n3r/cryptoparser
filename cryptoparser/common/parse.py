c0r0n3r/cryptolyzer

tls: Handshake protocol

crypton/common/parse.py
@@ -7,6 +7,7 @@ import struct
 import six
 
 from crypton.common.exception import NotEnoughData, TooMuchData, InvalidValue
+import crypton.common.utils as utils
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -65,6 +66,10 @@ class Parser(object):
     def unparsed_byte_num(self):
         return len(self._parsable_bytes) - self._parsed_byte_num
 
+    @property
+    def unparsed_bytes(self):
+        return self._parsable_bytes[self._parsed_byte_num:]
+
     def _parse_numeric_array(self, name, item_num, item_size, item_numeric_class):
         if self._parsed_byte_num + (item_num * item_size) > len(self._parsable_bytes):
             raise NotEnoughData(bytes_needed=(item_num * item_size) - self.unparsed_byte_num)
@@ -111,6 +116,53 @@ class Parser(object):
         self._parsed_byte_num += len(self._parsable_bytes) - self._parsed_byte_num - len(unparsed_bytes)
         self._parsed_values[name] = parsed_object
 
+    def _parse_parsable_array(self, name, items_size, item_classes, fallback_class=None):
+        if items_size > self.unparsed_byte_num:
+            raise NotEnoughData(bytes_needed=self._parsed_byte_num + items_size)
+
+        items = []
+        remaining_items_size = items_size
+
+        while remaining_items_size > 0:
+            for item_class in item_classes:
+                try:
+                    remaining_bytes_offset = self._parsed_byte_num + items_size - remaining_items_size
+                    item, parsed_byte_num = item_class._parse(self._parsable_bytes[remaining_bytes_offset:])
+                    break
+                except InvalidValue:
+                    pass
+            else:
+                if fallback_class is not None:
+                    remaining_bytes_offset = self._parsed_byte_num + items_size - remaining_items_size
+                    item, parsed_byte_num = fallback_class._parse(self._parsable_bytes[remaining_bytes_offset:])
+                else:
+                    raise ValueError(self._parsable_bytes[remaining_bytes_offset:])
+
+            items.append(item)
+            remaining_items_size -= parsed_byte_num
+
+        self._parsed_values[name] = items
+        self._parsed_byte_num += items_size
+
+    def parse_parsable_array(self, name, items_size, item_class):
+        if self.unparsed_byte_num < items_size:
+            raise NotEnoughData(items_size)
+
+        try:
+            return self._parse_parsable_array(name, items_size, [item_class, ])
+        except ValueError as e:
+            raise InvalidValue(e.args[0], item_class)
+
+
+    def parse_parsable_derived_array(self, name, items_size, item_base_class, fallback_class=None):
+        item_classes = utils.get_leaf_classes(item_base_class)
+        try:
+            return self._parse_parsable_array(name, items_size, item_classes, fallback_class)
+        except NotEnoughData as e:
+            raise e
+        except ValueError as e:
+            raise InvalidValue(e.args[0], item_base_class)
+
 
 class Composer(object):
     __INT_FORMATER_BY_SIZE = {
@@ -149,6 +201,14 @@ class Composer(object):
 
     def compose_parsable(self, value):
         self._composed_bytes += value.compose()
+
+    def compose_parsable_array(self, values):
+        composed_bytes = bytearray()
+
+        for item in values:
+            composed_bytes += item.compose()
+
+        self._composed_bytes += composed_bytes
 
     def compose_bytes(self, value):
         self._composed_bytes += value

tests/common/classes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from crypton.common.exception import TooMuchData, InvalidValue
 from crypton.common.parse import Parser, ParsableBase, Composer
 
 class NByteParsable(ParsableBase):
@@ -41,3 +42,55 @@ class OneByteParsable(NByteParsable):
 
 class TwoByteParsable(NByteParsable):
     BYTE_SIZE = 2
+
+
+class ConditionalParsable(ParsableBase):
+    def __init__(self, value):
+        self.value = value
+
+    def __int__(self):
+        return self.value
+
+    @classmethod
+    def _parse(cls, parsable_bytes):
+        parser = Parser(parsable_bytes)
+
+        parser.parse_numeric('value', cls.BYTE_SIZE)
+
+        cls.check_parsed(parser['value'])
+
+        return cls(parser['value']), cls.BYTE_SIZE
+
+    def compose(self):
+        composer = Composer()
+
+        composer.compose_numeric(self.value, self.BYTE_SIZE)
+
+        return composer.composed_bytes
+
+
+class OneByteOddParsable(ConditionalParsable):
+    BYTE_SIZE = 1
+
+    @classmethod
+    def check_parsed(cls, value):
+        if value % 2 == 0:
+            raise InvalidValue(value, OneByteOddParsable)
+
+
+class TwoByteEvenParsable(ConditionalParsable):
+    BYTE_SIZE = 2
+
+    @classmethod
+    def check_parsed(cls, value):
+        if value % 2 != 0:
+            raise InvalidValue(value, TwoByteEvenParsable)
+
+
+class AlwaysUnknowTypeParsable(ParsableBase):
+    @classmethod
+    def _parse(cls, parsable_bytes):
+        raise InvalidValue(parsable_bytes, AlwaysUnknowTypeParsable)
+
+    def compose(self):
+        raise TooMuchData()

tests/common/test_parse.py
@@ -6,7 +6,7 @@ import unittest
 from crypton.common.exception import NotEnoughData, TooMuchData, InvalidValue
 from crypton.common.parse import Parser, ParsableBase, Composer
 
-from tests.common.classes import OneByteParsable, TwoByteParsable
+from tests.common.classes import OneByteParsable, TwoByteParsable, ConditionalParsable
 
 
 class TestParsable(unittest.TestCase):
@@ -105,6 +105,73 @@ class TestParser(unittest.TestCase):
         parser.parse_bytes('two_byte_array', size=2)
         self.assertEqual(parser['two_byte_array'], b'\x01\x02')
 
+    def test_parse_parsable(self):
+        parser = Parser(b'\x01\x02\x03\x04')
+
+        parser.parse_parsable('first_byte', OneByteParsable)
+        self.assertEqual(
+            b'\x01',
+            parser['first_byte'].compose()
+        )
+
+        parser.parse_parsable('second_byte', OneByteParsable)
+        self.assertEqual(
+            b'\x02',
+            parser['second_byte'].compose()
+        )
+
+    def test_parse_parsable_array(self):
+        parser = Parser(b'\x01\x02\x03\x04')
+        parser.parse_parsable_array('array', items_size=4, item_class=OneByteParsable)
+        self.assertEqual(
+            [0x01, 0x02, 0x03, 0x04],
+            list(map(int, parser['array']))
+        )
+
+        parser = Parser(b'\x01\x02\x03\x04')
+        parser.parse_parsable_array('array', items_size=4, item_class=TwoByteParsable)
+        self.assertEqual(
+            [0x0102, 0x0304],
+            list(map(int, parser['array']))
+        )
+
+    def test_parse_parsable_derived_array(self):
+        parser = Parser(b'\x01\x02\x00')
+        parser.parse_parsable_derived_array(
+            'array',
+            items_size=3,
+            item_base_class=ConditionalParsable,
+            fallback_class=None
+        )
+        self.assertEqual(
+            [0x01, 0x0200],
+            list(map(int, parser['array']))
+        )
+        self.assertEqual(parser.unparsed_bytes, b'')
+
+        parser = Parser(b'\x00\x01')
+        with self.assertRaises(InvalidValue):
+            parser.parse_parsable_derived_array(
+                'array',
+                items_size=2,
+                item_base_class=ConditionalParsable,
+                fallback_class=None
+            )
+
+        parser = Parser(b'\x00\x01')
+        parser.parse_parsable_derived_array(
+            'array',
+            items_size=2,
+            item_base_class=ConditionalParsable,
+            fallback_class=TwoByteParsable
+        )
+        self.assertEqual(
+            [0x01, ],
+            list(map(int, parser['array']))
+        )
+        self.assertEqual(parser.unparsed_bytes, b'')
+
+
 class TestComposer(unittest.TestCase):
     def test_error(self):
         composer = Composer()
@@ -201,3 +268,13 @@ class TestComposer(unittest.TestCase):
         composer.compose_numeric(0x08090a0b, 4)
         self.assertEqual(composer.composed_bytes, b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b')
         self.assertEqual(composer.composed_byte_num, 11)
+
+    def test_compose_parsable_array(self):
+        composer = Composer()
+        parsable_array = [OneByteParsable(0x01), TwoByteParsable(0x0203), ]
+        composer.compose_parsable_array(parsable_array)
+
+        self.assertEqual(
+            b'\x01\x02\x03',
+            composer.composed_bytes
+        )

c0r0n3r/cryptoparser

cryptoparser/common/parse.py
@@ -7,6 +7,7 @@ import struct
 import six
 
 from cryptoparser.common.exception import NotEnoughData, TooMuchData, InvalidValue
+import cryptoparser.common.utils as utils
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -111,6 +112,52 @@ class ParserBinary(object):
         self._parsed_length += len(self._parsable) - self._parsed_length - len(unparsed_bytes)
         self._parsed_values[name] = parsed_object
 
+    def _parse_parsable_array(self, name, items_size, item_classes, fallback_class=None):
+        if items_size > self.unparsed_length:
+            raise NotEnoughData(bytes_needed=self._parsed_length + items_size)
+
+        items = []
+        remaining_items_size = items_size
+
+        while remaining_items_size > 0:
+            for item_class in item_classes:
+                try:
+                    remaining_bytes_offset = self._parsed_length + items_size - remaining_items_size
+                    item, parsed_length = item_class._parse(self._parsable[remaining_bytes_offset:])
+                    break
+                except InvalidValue:
+                    pass
+            else:
+                if fallback_class is not None:
+                    remaining_bytes_offset = self._parsed_length + items_size - remaining_items_size
+                    item, parsed_length = fallback_class._parse(self._parsable[remaining_bytes_offset:])
+                else:
+                    raise ValueError(self._parsable[remaining_bytes_offset:])
+
+            items.append(item)
+            remaining_items_size -= parsed_length
+
+        self._parsed_values[name] = items
+        self._parsed_length += items_size
+
+    def parse_parsable_array(self, name, items_size, item_class):
+        if self.unparsed_length < items_size:
+            raise NotEnoughData(items_size)
+
+        try:
+            return self._parse_parsable_array(name, items_size, [item_class, ])
+        except ValueError as e:
+            raise InvalidValue(e.args[0], item_class)
+
+    def parse_parsable_derived_array(self, name, items_size, item_base_class, fallback_class=None):
+        item_classes = utils.get_leaf_classes(item_base_class)
+        try:
+            return self._parse_parsable_array(name, items_size, item_classes, fallback_class)
+        except NotEnoughData as e:
+            raise e
+        except ValueError as e:
+            raise InvalidValue(e.args[0], item_base_class)
+
 
 class ComposerBinary(object):
     _INT_FORMATER_BY_SIZE = {
@@ -149,6 +196,14 @@ class ComposerBinary(object):
 
     def compose_parsable(self, value):
         self._composed += value.compose()
+
+    def compose_parsable_array(self, values):
+        composed_bytes = bytearray()
+
+        for item in values:
+            composed_bytes += item.compose()
+
+        self._composed += composed_bytes
 
     def compose_bytes(self, value):
         self._composed += value

tests/common/classes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from cryptoparser.common.exception import TooMuchData, InvalidValue
 from cryptoparser.common.parse import ParserBinary, ParsableBase, ComposerBinary
 
 
@@ -50,3 +51,59 @@ class TwoByteParsable(NByteParsable):
     @classmethod
     def get_byte_size(cls):
         return 2
+
+
+class ConditionalParsable(NByteParsable):
+    def __init__(self, value):
+        self.value = value
+
+    def __int__(self):
+        return self.value
+
+    @classmethod
+    def _parse(cls, parsable):
+        parser = ParserBinary(parsable)
+
+        parser.parse_numeric('value', cls.get_byte_size())
+
+        cls.check_parsed(parser['value'])
+
+        return cls(parser['value']), cls.get_byte_size()
+
+    def compose(self):
+        composer = ComposerBinary()
+
+        composer.compose_numeric(self.value, self.get_byte_size())
+
+        return composer.composed
+
+
+class OneByteOddParsable(ConditionalParsable):
+    @classmethod
+    def get_byte_size(cls):
+        return 1
+
+    @classmethod
+    def check_parsed(cls, value):
+        if value % 2 == 0:
+            raise InvalidValue(value, OneByteOddParsable)
+
+
+class TwoByteEvenParsable(ConditionalParsable):
+    @classmethod
+    def get_byte_size(cls):
+        return 2
+
+    @classmethod
+    def check_parsed(cls, value):
+        if value % 2 != 0:
+            raise InvalidValue(value, TwoByteEvenParsable)
+
+
+class AlwaysUnknowTypeParsable(ParsableBase):
+    @classmethod
+    def _parse(cls, parsable):
+        raise InvalidValue(parsable, AlwaysUnknowTypeParsable)
+
+    def compose(self):
+        raise TooMuchData()

tests/common/test_parse.py
@@ -6,7 +6,7 @@ import unittest
 from cryptoparser.common.exception import NotEnoughData, TooMuchData, InvalidValue
 from cryptoparser.common.parse import ParserBinary, ParsableBase, ComposerBinary
 
-from tests.common.classes import OneByteParsable
+from tests.common.classes import OneByteParsable, TwoByteParsable, ConditionalParsable
 
 
 class TestParsable(unittest.TestCase):
@@ -105,6 +105,72 @@ class TestParser(unittest.TestCase):
         parser.parse_bytes('two_byte_array', size=2)
         self.assertEqual(parser['two_byte_array'], b'\x01\x02')
 
+    def test_parse_parsable(self):
+        parser = ParserBinary(b'\x01\x02\x03\x04')
+
+        parser.parse_parsable('first_byte', OneByteParsable)
+        self.assertEqual(
+            b'\x01',
+            parser['first_byte'].compose()
+        )
+
+        parser.parse_parsable('second_byte', OneByteParsable)
+        self.assertEqual(
+            b'\x02',
+            parser['second_byte'].compose()
+        )
+
+    def test_parse_parsable_array(self):
+        parser = ParserBinary(b'\x01\x02\x03\x04')
+        parser.parse_parsable_array('array', items_size=4, item_class=OneByteParsable)
+        self.assertEqual(
+            [0x01, 0x02, 0x03, 0x04],
+            list(map(int, parser['array']))
+        )
+
+        parser = ParserBinary(b'\x01\x02\x03\x04')
+        parser.parse_parsable_array('array', items_size=4, item_class=TwoByteParsable)
+        self.assertEqual(
+            [0x0102, 0x0304],
+            list(map(int, parser['array']))
+        )
+
+    def test_parse_parsable_derived_array(self):
+        parser = ParserBinary(b'\x01\x02\x00')
+        parser.parse_parsable_derived_array(
+            'array',
+            items_size=3,
+            item_base_class=ConditionalParsable,
+            fallback_class=None
+        )
+        self.assertEqual(
+            [0x01, 0x0200],
+            list(map(int, parser['array']))
+        )
+        self.assertEqual(parser.unparsed_length, 0)
+
+        parser = ParserBinary(b'\x00\x01')
+        with self.assertRaises(InvalidValue):
+            parser.parse_parsable_derived_array(
+                'array',
+                items_size=2,
+                item_base_class=ConditionalParsable,
+                fallback_class=None
+            )
+
+        parser = ParserBinary(b'\x00\x01')
+        parser.parse_parsable_derived_array(
+            'array',
+            items_size=2,
+            item_base_class=ConditionalParsable,
+            fallback_class=TwoByteParsable
+        )
+        self.assertEqual(
+            [0x01, ],
+            list(map(int, parser['array']))
+        )
+        self.assertEqual(parser.unparsed_length, 0)
+
 
 class TestComposer(unittest.TestCase):
     def test_error(self):
@@ -202,3 +268,13 @@ class TestComposer(unittest.TestCase):
         composer.compose_numeric(0x08090a0b, 4)
         self.assertEqual(composer.composed, b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b')
         self.assertEqual(composer.composed_length, 11)
+
+    def test_compose_parsable_array(self):
+        composer = ComposerBinary()
+        parsable_array = [OneByteParsable(0x01), TwoByteParsable(0x0203), ]
+        composer.compose_parsable_array(parsable_array)
+
+        self.assertEqual(
+            b'\x01\x02\x03',
+            composer.composed
+        )