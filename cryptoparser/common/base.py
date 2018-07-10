73d1f8392de0aa519c784212953301def0ed6c3b
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-07-10 08:13:07 +0200

    fixup! JSON output

--- cryptoparser/common/base.py
+++ cryptoparser/common/base.py
@@ -19,8 +19,10 @@ def _default(self, obj):
         return { obj.name: obj.value._asdict() }
     elif isinstance(obj, JSONSerializable) and hasattr(obj, 'as_json'):
         return obj.as_json()
+    elif hasattr(obj, '__dict__'):
+        return { name: value for name, value in obj.__dict__.items() if not name.startswith('_') }
 
-    return { name: value for name, value in obj.__dict__.items() if not name.startswith('_') }
+    return str(obj)
 
 _default.default = json.JSONEncoder().default
 json.JSONEncoder.default = _default
