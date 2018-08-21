da6a1b465998daeab910f345c9bb8e3252f39311
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-08-21 19:31:23 +0200

    tls: named curve

--- cryptoparser/common/algorithm.py
+++ cryptoparser/common/algorithm.py
@@ -9,6 +9,7 @@ AuthenticationParams = collections.namedtuple('AuthenticationParams', ['name', '
 BlockCipherParams = collections.namedtuple('BlockCipherParams', ['name', 'key_size', 'block_size', 'exportable', ])
 BlockCipherModeParams = collections.namedtuple('BlockCipherModeParams', ['name', 'aead', ])
 MACParams = collections.namedtuple('MACParams', ['name', 'digest_size', ])
+NamedGroupParams = collections.namedtuple('NamedGroupParams', ['name', 'size', 'group_type', ])
 CipherSuiteParams = collections.namedtuple('TlsCipherSuiteParams', ['key_exchange', ])
 
 
@@ -298,4 +299,245 @@ class MAC(enum.Enum):
     SHA512 = MACParams(
         name='SHA512',
         digest_size=512
+    )
+
+
+class NamedGroupType(enum.IntEnum):
+    ELLIPTIC_CURVE = 1
+    DH_PARAM = 2
+
+
+class NamedGroup(enum.Enum):
+    SECT163K1 = NamedGroupParams(
+        name='sect163k1',
+        size=163,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT163R1 = NamedGroupParams(
+        name='sect163r1',
+        size=163,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT163R2 = NamedGroupParams(
+        name='sect163r2',
+        size=163,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT193R1 = NamedGroupParams(
+        name='sect193r1',
+        size=193,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT193R2 = NamedGroupParams(
+        name='sect193r2',
+        size=193,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT233K1 = NamedGroupParams(
+        name='sect233k1',
+        size=233,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT233R1 = NamedGroupParams(
+        name='sect233r1',
+        size=233,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT239K1 = NamedGroupParams(
+        name='sect239k1',
+        size=239,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT283K1 = NamedGroupParams(
+        name='sect283k1',
+        size=283,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT283R1 = NamedGroupParams(
+        name='sect283r1',
+        size=283,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT409K1 = NamedGroupParams(
+        name='sect409k1',
+        size=409,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT409R1 = NamedGroupParams(
+        name='sect409r1',
+        size=409,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT571K1 = NamedGroupParams(
+        name='sect571k1',
+        size=571,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECT571R1 = NamedGroupParams(
+        name='sect571r1',
+        size=571,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP160K1 = NamedGroupParams(
+        name='secp160k1',
+        size=160,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP160R1 = NamedGroupParams(
+        name='secp160r1',
+        size=160,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP160R2 = NamedGroupParams(
+        name='secp160r2',
+        size=160,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP192K1 = NamedGroupParams(
+        name='secp192k1',
+        size=192,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP192R1 = NamedGroupParams(
+        name='secp192r1',
+        size=192,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP224K1 = NamedGroupParams(
+        name='secp224k1',
+        size=224,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP224R1 = NamedGroupParams(
+        name='secp224r1',
+        size=224,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP256K1 = NamedGroupParams(
+        name='secp256k1',
+        size=256,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP256R1 = NamedGroupParams(
+        name='secp256r1',
+        size=256,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP384R1 = NamedGroupParams(
+        name='secp384r1',
+        size=384,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    SECP521R1 = NamedGroupParams(
+        name='secp521r1',
+        size=521,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+
+    BRAINPOOLP160R1 = NamedGroupParams(
+        name='brainpoolp160r1',
+        size=160,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP160T1 = NamedGroupParams(
+        name='brainpoolp160t1',
+        size=160,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP192R1 = NamedGroupParams(
+        name='brainpoolp192r1',
+        size=192,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP192T1 = NamedGroupParams(
+        name='brainpoolp192t1',
+        size=192,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP224R1 = NamedGroupParams(
+        name='brainpoolp224r1',
+        size=224,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP224T1 = NamedGroupParams(
+        name='brainpoolp224t1',
+        size=224,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP256R1 = NamedGroupParams(
+        name='brainpoolp256r1',
+        size=256,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP256T1 = NamedGroupParams(
+        name='brainpoolp256t1',
+        size=256,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP320R1 = NamedGroupParams(
+        name='brainpoolp320r1',
+        size=320,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP320T1 = NamedGroupParams(
+        name='brainpoolp320t1',
+        size=320,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP384R1 = NamedGroupParams(
+        name='brainpoolp384r1',
+        size=384,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP384T1 = NamedGroupParams(
+        name='brainpoolp384t1',
+        size=384,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP512R1 = NamedGroupParams(
+        name='brainpoolp512r1',
+        size=512,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    BRAINPOOLP512T1 = NamedGroupParams(
+        name='brainpoolp512t1',
+        size=512,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+
+    CURVE25519 = NamedGroupParams(
+        name='curve25519',
+        size=256,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+    CURVE448 = NamedGroupParams(
+        name='curve448',
+        size=448,
+        group_type=NamedGroupType.ELLIPTIC_CURVE,
+    )
+
+    FFDHE2048 = NamedGroupParams(
+        name='ffdhe2048',
+        size=2048,
+        group_type=NamedGroupType.DH_PARAM,
+    )
+    FFDHE3072 = NamedGroupParams(
+        name='ffdhe3072',
+        size=3072,
+        group_type=NamedGroupType.DH_PARAM,
+    )
+    FFDHE4096 = NamedGroupParams(
+        name='ffdhe4096',
+        size=4096,
+        group_type=NamedGroupType.DH_PARAM,
+    )
+    FFDHE6144 = NamedGroupParams(
+        name='ffdhe6144',
+        size=6144,
+        group_type=NamedGroupType.DH_PARAM,
+    )
+    FFDHE8192 = NamedGroupParams(
+        name='ffdhe8192',
+        size=8192,
+        group_type=NamedGroupType.DH_PARAM,
     )
