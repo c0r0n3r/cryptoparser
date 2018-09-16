c0r0n3r/cryptolyzer

tls: record protocol

crypton/tls/record.py
@@ -1,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import crypton.common.utils as utils
+
+from crypton.common.parse import ParsableBase, Parser, Composer
+from crypton.common.exception import NotEnoughData, InvalidValue
+from crypton.tls.version import TlsVersion, TlsProtocolVersionBase, TlsProtocolVersionFinal
+from crypton.tls.subprotocol import TlsSubprotocolMessageBase, TlsContentType
+
+
+class TlsRecord(ParsableBase):
+    def __init__(self, messages, protocol_version=TlsProtocolVersionFinal(TlsVersion.TLS1_2)):
+        # type: (TlsProtocolVersionBase, TlsSubprotocolMessageBase) -> None
+        self.protocol_version = protocol_version
+        self.messages = messages
+
+    @classmethod
+    def _parse(cls, parsable_bytes):
+        parser = Parser(parsable_bytes)
+
+        try:
+            parser.parse_numeric('content_type', 1, TlsContentType)
+        except InvalidValue as e:
+            raise InvalidValue(e.value, TlsContentType)
+        parser.parse_parsable('protocol_version', TlsProtocolVersionBase)
+        parser.parse_numeric('record_length', 2)
+
+        if parser.parsed_byte_num + parser['record_length'] > len(parsable_bytes):
+            raise NotEnoughData(parser['record_length'])
+
+        header_size = parser.parsed_byte_num
+
+        messages = []
+        while parser.parsed_byte_num < parser['record_length'] + header_size:
+            for subclass in utils.get_leaf_classes(TlsSubprotocolMessageBase):
+                if subclass.get_content_type() != parser['content_type']:
+                    continue
+
+                try:
+                    parser.parse_parsable('message', subclass)
+                    messages.append(parser['message'])
+                    break
+                except InvalidValue:
+                    continue
+            else:
+                raise InvalidValue(parser['content_type'], TlsRecord, 'content type')
+
+        return TlsRecord(messages=messages, protocol_version=parser['protocol_version']), parser.parsed_byte_num
+
+    def compose(self):
+        body_composer = Composer()
+        body_composer.compose_parsable(self.messages[0])
+
+        header_composer = Composer()
+        content_type = self.messages[0].get_content_type()
+        header_composer.compose_numeric(content_type, 1)
+        header_composer.compose_parsable(self.protocol_version)
+        header_composer.compose_numeric(body_composer.composed_byte_num, 2)
+
+        return header_composer.composed_bytes + body_composer.composed_bytes
+
+    @property
+    def protocol_version(self):
+        return self._protocol_version
+
+    @protocol_version.setter
+    def protocol_version(self, value):
+        if not isinstance(value, TlsProtocolVersionBase):
+            raise ValueError()
+
+        # pylint: disable=attribute-defined-outside-init
+        self._protocol_version = value
+
+    @property
+    def messages(self):
+        return self._messages
+
+    @messages.setter
+    def messages(self, value):
+        if not all([issubclass(type(item), TlsSubprotocolMessageBase) for item in value]):
+            raise ValueError()
+
+        # pylint: disable=attribute-defined-outside-init
+        self._messages = value

crypton/tls/subprotocol.py
@@ -1,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+import enum
+
+from crypton.common.exception import NotEnoughData, InvalidValue
+from crypton.common.parse import ParsableBase, Parser, Composer
+
+
+class TlsContentType(enum.IntEnum):
+    CHANGE_CIPHER_SPEC = 0x14
+    ALERT = 0x15
+    HANDSHAKE = 0x16
+    APPLICATION_DATA = 0x17
+    HEARTBEAT = 0x18
+
+
+class TlsSubprotocolMessageBase(ParsableBase):
+    @classmethod
+    @abc.abstractmethod
+    def get_content_type(cls):
+        raise NotImplementedError()
+
+
+class TlsAlertLevel(enum.IntEnum):
+    WARNING = 0x01
+    FATAL = 0x02
+
+
+class TlsAlertDescription(enum.IntEnum):
+    CLOSE_NOTIFY = 0x00
+    UNEXPECTED_MESSAGE = 0x0a
+    BAD_RECORD_MAC = 0x14
+    RECORD_OVERFLOW = 0x16
+    HANDSHAKE_FAILURE = 0x28
+    BAD_CERTIFICATE = 0x2a
+    UNSUPPORTED_CERTIFICATE = 0x2b
+    CERTIFICATE_REVOKED = 0x2c
+    CERTIFICATE_EXPIRED = 0x2d
+    CERTIFICATE_UNKNOWN = 0x2e
+    ILLEGAL_PARAMETER = 0x2f
+    UNKNOWN_CA = 0x30
+    ACCESS_DENIED = 0x30
+    DECODE_ERROR = 0x32
+    DECRYPT_ERROR = 0x33
+    PROTOCOL_VERSION = 0x46
+    INSUFFICIENT_SECURITY = 0x47
+    INTERNAL_ERROR = 0x50
+    INAPPROPRIATE_FALLBACK = 0x56
+    USER_CANCELED = 0x5a
+    MISSING_EXTENSION = 0x6d
+    UNSUPPORTED_EXTENSION = 0x6e
+    CERTIFICATE_UNOBTAINABLE = 0x6f
+    UNRECOGNIZED_NAME = 0x70
+    BAD_CERTIFICATE_STATUS_RESPONSE = 0x71
+    BAD_CERTIFICATE_HASH_VALUE = 0x72
+    UNKNOWN_PSK_IDENTITY = 0x73
+    CERTIFICATE_REQUIRED = 0x74
+    NO_APPLICATION_PROTOCOL = 0x78
+
+
+class TlsAlertMessage(TlsSubprotocolMessageBase):
+    __SIZE = 2
+
+    def __init__(self, level, description):
+        self.level = level
+        self.description = description
+
+    @classmethod
+    def get_content_type(cls):
+        return TlsContentType.ALERT
+
+    @classmethod
+    def _parse(cls, parsable_bytes):
+        if len(parsable_bytes) < cls.__SIZE:
+            raise NotEnoughData(cls.__SIZE)
+
+        parser = Parser(parsable_bytes)
+
+        parser.parse_numeric('level', 1)
+        parser.parse_numeric('description', 1)
+
+        return TlsAlertMessage(parser['level'], parser['description']), parser.parsed_byte_num
+
+    def compose(self):
+        composer = Composer()
+
+        composer.compose_numeric(self.level, 1)
+        composer.compose_numeric(self.description, 1)
+
+        return composer.composed_bytes
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        try:
+            # pylint: disable=attribute-defined-outside-init
+            self._level = TlsAlertLevel(value)
+        except ValueError:
+            raise InvalidValue(value, TlsAlertLevel)
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        try:
+            # pylint: disable=attribute-defined-outside-init
+            self._description = TlsAlertDescription(value)
+        except ValueError:
+            raise InvalidValue(value, TlsAlertDescription)
+
+    def __eq__(self, other):
+        return self.level == other.level and self.description == other.description

tests/tls/test_alert.py
@@ -1,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import six
+
+from crypton.common.exception import NotEnoughData, InvalidValue
+
+from crypton.tls.subprotocol import TlsAlertMessage, TlsAlertLevel, TlsAlertDescription
+
+
+class TestRecord(unittest.TestCase):
+    def test_error(self):
+        with six.assertRaisesRegex(self, InvalidValue, '0xff is not a valid TlsAlertLevel'):
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_bytes(b'\xff\x00'),
+
+        with six.assertRaisesRegex(self, InvalidValue, '0xff is not a valid TlsAlertDescription'):
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_bytes(b'\x01\xff'),
+
+        with self.assertRaises(NotEnoughData) as context_manager:
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_bytes(b'\xff'),
+        self.assertGreater(context_manager.exception.bytes_needed, 1)
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsAlertMessage.parse_exact_bytes(b'\x02\x28'),
+            TlsAlertMessage(TlsAlertLevel.FATAL, TlsAlertDescription.HANDSHAKE_FAILURE)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            b'\x02\x28',
+            TlsAlertMessage(TlsAlertLevel.FATAL, TlsAlertDescription.HANDSHAKE_FAILURE).compose()
+        )

c0r0n3r/cryptoparser

cryptoparser/tls/record.py
@@ -1,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cryptoparser.common.utils as utils
+
+from cryptoparser.common.parse import ParsableBase, ParserBinary, ComposerBinary
+from cryptoparser.common.exception import NotEnoughData, InvalidValue
+from cryptoparser.tls.version import TlsVersion, TlsProtocolVersionBase, TlsProtocolVersionFinal
+from cryptoparser.tls.subprotocol import TlsSubprotocolMessageBase, TlsContentType
+
+
+class TlsRecord(ParsableBase):
+    def __init__(self, messages, protocol_version=TlsProtocolVersionFinal(TlsVersion.TLS1_2)):
+        # type: (TlsProtocolVersionBase, TlsSubprotocolMessageBase) -> None
+        self.protocol_version = protocol_version
+        self.messages = messages
+
+    @classmethod
+    def _parse(cls, parsable):
+        parser = ParserBinary(parsable)
+
+        try:
+            parser.parse_numeric('content_type', 1, TlsContentType)
+        except InvalidValue as e:
+            raise InvalidValue(e.value, TlsContentType)
+        parser.parse_parsable('protocol_version', TlsProtocolVersionBase)
+        parser.parse_numeric('record_length', 2)
+
+        if parser.parsed_length + parser['record_length'] > len(parsable):
+            raise NotEnoughData(parser['record_length'])
+
+        header_size = parser.parsed_length
+
+        messages = []
+        while parser.parsed_length < parser['record_length'] + header_size:
+            for subclass in utils.get_leaf_classes(TlsSubprotocolMessageBase):
+                if subclass.get_content_type() != parser['content_type']:
+                    continue
+
+                try:
+                    parser.parse_parsable('message', subclass)
+                    messages.append(parser['message'])
+                    break
+                except InvalidValue:
+                    continue
+            else:
+                raise InvalidValue(parser['content_type'], TlsRecord, 'content type')
+
+        return TlsRecord(messages=messages, protocol_version=parser['protocol_version']), parser.parsed_length
+
+    def compose(self):
+        body_composer = ComposerBinary()
+        body_composer.compose_parsable(self.messages[0])
+
+        header_composer = ComposerBinary()
+        content_type = self.messages[0].get_content_type()
+        header_composer.compose_numeric(content_type, 1)
+        header_composer.compose_parsable(self.protocol_version)
+        header_composer.compose_numeric(body_composer.composed_length, 2)
+
+        return header_composer.composed_bytes + body_composer.composed_bytes
+
+    @property
+    def protocol_version(self):
+        return self._protocol_version
+
+    @protocol_version.setter
+    def protocol_version(self, value):
+        if not isinstance(value, TlsProtocolVersionBase):
+            raise ValueError()
+
+        # pylint: disable=attribute-defined-outside-init
+        self._protocol_version = value
+
+    @property
+    def messages(self):
+        return self._messages
+
+    @messages.setter
+    def messages(self, value):
+        if not all([issubclass(type(item), TlsSubprotocolMessageBase) for item in value]):
+            raise ValueError()
+
+        # pylint: disable=attribute-defined-outside-init
+        self._messages = value

cryptoparser/tls/subprotocol.py
@@ -1,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+import enum
+
+from cryptoparser.common.exception import NotEnoughData, InvalidValue
+from cryptoparser.common.parse import ParsableBase, ParserBinary, ComposerBinary
+
+
+class TlsContentType(enum.IntEnum):
+    CHANGE_CIPHER_SPEC = 0x14
+    ALERT = 0x15
+    HANDSHAKE = 0x16
+    APPLICATION_DATA = 0x17
+    HEARTBEAT = 0x18
+
+
+class TlsSubprotocolMessageBase(ParsableBase):
+    @abc.abstractmethod
+    def get_content_type(cls):
+        raise NotImplementedError()
+
+
+class TlsAlertLevel(enum.IntEnum):
+    WARNING = 0x01
+    FATAL = 0x02
+
+
+class TlsAlertDescription(enum.IntEnum):
+    CLOSE_NOTIFY = 0x00
+    UNEXPECTED_MESSAGE = 0x0a
+    BAD_RECORD_MAC = 0x14
+    RECORD_OVERFLOW = 0x16
+    HANDSHAKE_FAILURE = 0x28
+    BAD_CERTIFICATE = 0x2a
+    UNSUPPORTED_CERTIFICATE = 0x2b
+    CERTIFICATE_REVOKED = 0x2c
+    CERTIFICATE_EXPIRED = 0x2d
+    CERTIFICATE_UNKNOWN = 0x2e
+    ILLEGAL_PARAMETER = 0x2f
+    UNKNOWN_CA = 0x30
+    ACCESS_DENIED = 0x30
+    DECODE_ERROR = 0x32
+    DECRYPT_ERROR = 0x33
+    PROTOCOL_VERSION = 0x46
+    INSUFFICIENT_SECURITY = 0x47
+    INTERNAL_ERROR = 0x50
+    INAPPROPRIATE_FALLBACK = 0x56
+    USER_CANCELED = 0x5a
+    MISSING_EXTENSION = 0x6d
+    UNSUPPORTED_EXTENSION = 0x6e
+    CERTIFICATE_UNOBTAINABLE = 0x6f
+    UNRECOGNIZED_NAME = 0x70
+    BAD_CERTIFICATE_STATUS_RESPONSE = 0x71
+    BAD_CERTIFICATE_HASH_VALUE = 0x72
+    UNKNOWN_PSK_IDENTITY = 0x73
+    CERTIFICATE_REQUIRED = 0x74
+    NO_APPLICATION_PROTOCOL = 0x78
+
+
+class TlsAlertMessage(TlsSubprotocolMessageBase):
+    _SIZE = 2
+
+    def __init__(self, level, description):
+        self.level = level
+        self.description = description
+
+    @classmethod
+    def get_content_type(cls):
+        return TlsContentType.ALERT
+
+    @classmethod
+    def _parse(cls, parsable):
+        if len(parsable) < cls._SIZE:
+            raise NotEnoughData(cls._SIZE - len(parsable))
+
+        parser = ParserBinary(parsable)
+
+        parser.parse_numeric('level', 1)
+        parser.parse_numeric('description', 1)
+
+        return TlsAlertMessage(parser['level'], parser['description']), parser.parsed_length
+
+    def compose(self):
+        composer = ComposerBinary()
+
+        composer.compose_numeric(self.level, 1)
+        composer.compose_numeric(self.description, 1)
+
+        return composer.composed_bytes
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        try:
+            # pylint: disable=attribute-defined-outside-init
+            self._level = TlsAlertLevel(value)
+        except ValueError:
+            raise InvalidValue(value, TlsAlertLevel)
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        try:
+            # pylint: disable=attribute-defined-outside-init
+            self._description = TlsAlertDescription(value)
+        except ValueError:
+            raise InvalidValue(value, TlsAlertDescription)
+
+    def __eq__(self, other):
+        return self.level == other.level and self.description == other.description

tests/tls/test_alert.py
@@ -1,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import six
+
+from cryptoparser.common.exception import NotEnoughData, InvalidValue
+
+from cryptoparser.tls.subprotocol import TlsAlertMessage, TlsAlertLevel, TlsAlertDescription
+
+
+class TestAlert(unittest.TestCase):
+    def test_error(self):
+        with six.assertRaisesRegex(self, InvalidValue, '0xff is not a valid TlsAlertLevel'):
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_size(b'\xff\x00'),
+
+        with six.assertRaisesRegex(self, InvalidValue, '0xff is not a valid TlsAlertDescription'):
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_size(b'\x01\xff'),
+
+        with self.assertRaises(NotEnoughData) as context_manager:
+            # pylint: disable=expression-not-assigned
+            TlsAlertMessage.parse_exact_size(b'\xff'),
+        self.assertGreaterEqual(context_manager.exception.bytes_needed, 1)
+
+    def test_parse(self):
+        self.assertEqual(
+            TlsAlertMessage.parse_exact_size(b'\x02\x28'),
+            TlsAlertMessage(TlsAlertLevel.FATAL, TlsAlertDescription.HANDSHAKE_FAILURE)
+        )
+
+    def test_compose(self):
+        self.assertEqual(
+            b'\x02\x28',
+            TlsAlertMessage(TlsAlertLevel.FATAL, TlsAlertDescription.HANDSHAKE_FAILURE).compose()
+        )