324da111f4e72af289d679d638c5256007e147ae
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2021-10-07 09:07:54 +0200

    chore(setup)!: Bump version number to 0.7.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def test_discover():
 
 setup(
     name='cryptoparser',
-    version='0.7.1',
+    version='0.7.2',
     description='Fast and flexible security protocol parser and generator',
     long_description=long_description,
     long_description_content_type='text/markdown',
