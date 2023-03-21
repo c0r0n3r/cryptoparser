1b5ec47d25a12feb06c8ef90b801b7e89c6e6d4f
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-03-22 00:46:43 +0100

    docs: Initial commit of automatically generated documentation (#69)

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import datetime
+
+__author__ = 'Szilárd Pfeiffer'
+__title__ = 'CryptoParser'
+
+
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = __title__
+copyright = f'{datetime.datetime.now().year}, {__author__}'
+
+exclude_patterns = ['_build']
+
+html_theme = 'alabaster'
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
+html_theme_options = {
+    'description': 'Cryptographic protocol and security-related protocol piece parser',
+    'fixed_sidebar': True,
+    'collapse_navigation': False,
+}
---
+++ docs/development.rst
@@ -0,0 +1,10 @@
+Development
+-----------
+
+If you want to setup a development environment, you are in need of `pipenv <https://docs.pipenv.org/>`__.
+
+.. code:: shell
+
+   $ cd cryptoparser
+   $ pipenv install --dev
+   $ pipenv shell
