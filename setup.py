11cead231dc7f30a3ec115fccc3df8ef6154c03c
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-04-29 13:23:52 +0200

    chore(release): Bump version number to 0.9.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def test_discover():
 
 setup(
     name='cryptoparser',
-    version='0.8.5',
+    version='0.9.0',
     description='Fast and flexible security protocol parser and generator',
     long_description=long_description,
     long_description_content_type='text/x-rst',
